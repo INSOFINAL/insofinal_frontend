ec239fa48b5ff607a15e93357a38968fb899d126
Espinozaje <jeisson12aaron@gmail.com>
2024-12-01 11:37:38 -0500

    conexion backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080/'   // URL base para producción
+    apiUrl: 'https://insoparcialapi-inso.up.railway.app/'   // URL base para producción
   };
